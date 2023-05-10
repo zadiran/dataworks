698fbf1d4df27438520024a942db140417957a10
zadiran <konstantin.zadiran@gmail.com>
2023-05-10 20:20:03 +0300

    Refactoring

--- run_point_forecast_binary.py
+++ run_point_forecast_binary.py
@@ -1,20 +1,15 @@
-from typing import List
 from sklearn.metrics import roc_auc_score, f1_score, accuracy_score, precision_score
 
 import matplotlib.pyplot as plt
 
-from data_processing.nasa_data_source import nasa_data_source
-from data_processing.nasa_data_manipulator import nasa_data_manipulator
-from models.baseline_binary_forecast_model import baseline_binary_forecast_model
 from models.baseline_binary_point_forecast_model import baseline_binary_point_forecast_model
-from utils.point import point
-from data_processing.binary_point_data_source import binary_point_data_source as bpds
+from data_processing.binary_point_data_source import binary_point_data_source
 
 from utils.splitter import splitter
 
 window_size = 10
 
-data = bpds().get_data('data/train_FD001.txt', window_size)
+data = binary_point_data_source().get_data('data/train_FD001.txt', window_size)
 
 def split_func(training_set, verification_set):
     bbpfm = baseline_binary_point_forecast_model()
