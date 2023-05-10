b35f2f380e79f64b7f542369ec1c57ce208afeaf
zadiran <konstantin.zadiran@gmail.com>
2023-05-10 20:18:34 +0300

    Fixed example script names

--- run_forecast_binary.py
+++ run_forecast_binary.py
@@ -1,4 +1,3 @@
-from typing import List
 from sklearn.metrics import roc_auc_score, f1_score, accuracy_score, precision_score
 
 import matplotlib.pyplot as plt
@@ -6,31 +5,33 @@ import matplotlib.pyplot as plt
 from data_processing.nasa_data_source import nasa_data_source
 from data_processing.nasa_data_manipulator import nasa_data_manipulator
 from models.baseline_binary_forecast_model import baseline_binary_forecast_model
-from models.baseline_binary_point_forecast_model import baseline_binary_point_forecast_model
-from utils.point import point
-from data_processing.binary_point_data_source import binary_point_data_source as bpds
 
 from utils.splitter import splitter
 
-window_size = 10
+ds = nasa_data_source('data/train_FD001.txt')
+dm = nasa_data_manipulator(ds)
 
-data = bpds().get_data('data/train_FD001.txt', window_size)
+output = []
+expected = []
 
-def split_func(training_set, verification_set):
-    bbpfm = baseline_binary_point_forecast_model()
-    bbpfm.fit(training_set)
-    bbpfm.predict_points(verification_set)
 
-splitter().run(split_func, data, 5, [])
+for i in range(0,5):
+    fm = baseline_binary_forecast_model(dm)
+    
+    dm.set_cv_range(i * 20 + 1, (i+1) * 20 + 1)
+    fm.fit()
 
-forecasted = list(map(lambda x: x.forecasted_output, data))
-expected = list(map(lambda x: x.training_output, data))
+    for row in dm.get_clean_testing_input().to_numpy():
+        output.append(fm.predict(row)[0])
 
-print('ROC/AUC: ' + str(roc_auc_score(forecasted, expected)))
-print('F1: ' + str(f1_score(forecasted, expected)))
-print('Accuracy: ' + str(accuracy_score(forecasted, expected)))
-print('Precision: ' + str(precision_score(forecasted, expected)))
+    expected +=  dm.get_logical_proximity(dm.get_cv_testing_output(), 10)
+
+
+print('ROC/AUC: ' + str(roc_auc_score(output, expected)))
+print('F1: ' + str(f1_score(output, expected)))
+print('Accuracy: ' + str(accuracy_score(output, expected)))
+print('Precision: ' + str(precision_score(output, expected)))
 
 plt.plot(expected[:500], color = 'orange')
-plt.scatter(range(0,500), forecasted[:500])
+plt.scatter(range(0,500), output[:500])
 plt.show()
